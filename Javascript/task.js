25727a1f14e5baccd2d7b5be3cced79517106148
Yoefkada <karadayoussef10@gmail.com>
2024-10-25 12:58:57 +0100

    js

--- Javascript/task.js
+++ Javascript/task.js
@@ -16,12 +16,71 @@
 
 // Third task
 
-let birthday = 2003;
+//let birthday = 2003;
 
-let currentYear = 2024;
+//let currentYear = 2024;
 
-let age = currentYear - birthday;
+//let age = currentYear - birthday;
 
-console.log("It will be either " + (age - 1) + " or " + age);
+//console.log("It will be either " + (age - 1) + " or " + age);
 
 // Fourth task
+
+//let odds = [];
+//let even = [];
+
+//for(let i = 0; i <= 10; i++) {
+    //if(i % 2 === 0) {
+        //even.push(i);
+    //} else {
+        //odds.push(i);
+    //}
+//}
+
+//console.log("Even numbers: ", even);
+
+//console.log("Odd numbers: ", odds);
+
+// Fifth task
+
+//console.log(2 + 2 + '1')
+//console.log('1' + 2 + 2)
+
+// Sixth task
+
+//let apples = "2";
+//let oranges = "3";
+
+//console.log(apples + oranges);
+
+// Seventh task
+
+//let x = 2 * 2 + 1
+
+//console.log(x)
+
+// Eighth task
+
+//let a, b, c;
+//a = b = c = 2 + 2;
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+// Ninth task
+
+//let counter = 2;
+
+//counter++;
+
+//console.log(counter);
+
+//let counter = 2;
+
+//counter--;
+
+//console.log(counter);
+
+// Tenth task
+
