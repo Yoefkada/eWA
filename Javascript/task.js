248f5fd3eb03e8fd22867f7348b2790713f5e060
Yoefkada <karadayoussef10@gmail.com>
2024-10-25 12:19:09 +0100

    js

---
+++ Javascript/task.js
@@ -0,0 +1,27 @@
+// First task 
+
+//let admin;
+//let name;
+
+//name = "John";
+//admin = name;
+
+//console.log(admin)
+
+// Second task
+
+//let ourplanetname = "Earth";
+
+//let currentusername = "John";
+
+// Third task
+
+let birthday = 2003;
+
+let currentYear = 2024;
+
+let age = currentYear - birthday;
+
+console.log("It will be either " + (age - 1) + " or " + age);
+
+// Fourth task
